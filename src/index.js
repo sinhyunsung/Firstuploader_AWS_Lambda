8c5970eec930d2cf818a4ae623116a3d1fad5965
sinhyunsung <hyunsungsung@naver.com>
2023-04-11 12:47:34 +0900

    test

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ module.exports.handler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Hello World!"
+      message: "Hello Worldsss!"
     })
   }
 
