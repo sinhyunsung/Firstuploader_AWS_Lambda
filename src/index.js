b0c451df267e156801244b547b024cdb1061efe0
sinhyunsung <hyunsungsung@naver.com>
2023-04-13 18:10:57 +0900

    update: mlb 추가 테스트

--- src/index.js
+++ src/index.js
@@ -2,22 +2,58 @@ const chromium = require('chrome-aws-lambda');
 
 module.exports.handler = async (event, context) => {
 
-  const browser = await chromium.puppeteer.launch({
-    executablePath: await chromium.executablePath,
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    headless: chromium.headless,
-  });
+  const web=new Mlb("https://24hz.kr/board/bbs/link.php?bo_table=mlbpark&wr_id=8798179&no=1");
+  
+  await web.chrome_on();
+  await web.page_on();
+  await web.page_goto();
+  const comments = await web.crawling();
+  await web.chrome_close();
 
-  const page = await browser.newPage();
-
-  await browser.close();
   //// setTimeout(() => chrome.instance.kill(), 0);
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "테스트완료 삐빅"
+      message: comments
     })
   }
 
+}
+
+
+class Crawling{
+    constructor(url) {
+      this.url=url;
+      this.browser;
+      this.page;
+    }
+
+    async chrome_on(){
+      this.browser = await chromium.puppeteer.launch({
+        executablePath: await chromium.executablePath,
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        headless: true,
+      });
+    }
+    
+    async chrome_close(){  
+      await this.browser.close();
+    }
+
+    async page_on(){
+      this.page = await this.browser.newPage();
+    }
+
+    async page_goto(){
+      await this.page.goto(this.url);
+    }
+}
+
+class Mlb extends Crawling{
+    
+    async crawling(){
+        const comments = await this.page.$$eval('.re_txt', comments => comments.map(comment => comment.innerText));
+        return comments;
+    }
 }
