e0468f7c340d71fcb8d321730d1a7836c23bbff3
sinhyunsung <hyunsungsung@naver.com>
2023-04-17 12:14:31 +0900

    test

--- src/index.js
+++ src/index.js
@@ -116,57 +116,42 @@ const classMap = {
 
 // ############################### 메인 코드 ############################//
 
-module.exports.handler = async (event, context, callback) => {
-  const params = event;
+
+module.exports.handler = async (event, context) => {
+
   
-  // Do something with the parameters...
+  const body = JSON.stringify(event["body-json"]);
+
+  const url= event.queryStringParameters.url;
+  const words = url.split('/')[2];
+  const ClassConstructor = classMap[words];
+
+  let web=null;
+  // Create an instance of the class constructor if it exists
+  if (ClassConstructor) {
+    web = new ClassConstructor(url);
+    // do something with web...
+  }else{
+    return {
+      statusCode: 403,
+      message: "데이터에 없는 url 입니다. ("+words+")"
+    }
+  }
   
+  
+  await web.chrome_on();
+  await web.page_on();
+  await web.page_goto();
+  const comments = await web.crawling();
+  await web.chrome_close();
+
+  setTimeout(() => chrome.instance.kill(), 0);
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: params
+      message: comments,
     })
   }
-};
-// module.exports.handler = async (event, context) => {
-
-  
-//   const body = JSON.stringify(event["body-json"]);
-
-//   // const url= event.params.path.url;
-//   // const words = url.split('/')[2];
-//   // const ClassConstructor = classMap[words];
-
-//   // let web=null;
-//   // // Create an instance of the class constructor if it exists
-//   // if (ClassConstructor) {
-//   //   web = new ClassConstructor(url);
-//   //   // do something with web...
-//   // }else{
-//   //   return {
-//   //     statusCode: 403,
-//   //     message: "데이터에 없는 url 입니다. ("+words+")"
-//   //   }
-//   // }
-  
-  
-//   // await web.chrome_on();
-//   // await web.page_on();
-//   // await web.page_goto();
-//   // const comments = await web.crawling();
-//   // await web.chrome_close();
-
-//   //setTimeout(() => chrome.instance.kill(), 0);
-//  return {
-//   statusCode: 200,
-//   body: body
-//   };
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify({
-//       message: comments,
-//       msg: event.params.path.url
-//     })
-//   }
-// }
+}
 
