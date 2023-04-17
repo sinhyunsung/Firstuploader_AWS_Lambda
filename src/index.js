6b597d7c1ddd5737db326e7d5ee5441adb7a6928
sinhyunsung <hyunsungsung@naver.com>
2023-04-17 11:32:23 +0900

    test

--- src/index.js
+++ src/index.js
@@ -123,7 +123,6 @@ exports.handler = async (event, context, callback) => {
   
   callback(null, {
     statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ result: event })
   });
 };
