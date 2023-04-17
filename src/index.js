355bf8ae351ecddd709641017f21b16625a035eb
sinhyunsung <hyunsungsung@naver.com>
2023-04-17 14:02:41 +0900

    test

--- src/index.js
+++ src/index.js
@@ -11,40 +11,40 @@ class Crawling{
   async chrome_on(){
     const minimal_args = [
       '--autoplay-policy=user-gesture-required',
-      // '--disable-background-networking',
-      // '--disable-background-timer-throttling',
-      // '--disable-backgrounding-occluded-windows',
-      // '--disable-breakpad',
-      // '--disable-client-side-phishing-detection',
-      // '--disable-component-update',
-      // '--disable-default-apps',
-      // '--disable-dev-shm-usage',
-      // '--disable-domain-reliability',
-      // '--disable-extensions',
-      // '--disable-features=AudioServiceOutOfProcess',
-      // '--disable-hang-monitor',
-      // '--disable-ipc-flooding-protection',
-      // '--disable-notifications',
-      // '--disable-offer-store-unmasked-wallet-cards',
-      // '--disable-popup-blocking',
-      // '--disable-print-preview',
-      // '--disable-prompt-on-repost',
-      // '--disable-renderer-backgrounding',
-      // '--disable-setuid-sandbox',
-      // '--disable-speech-api',
-      // '--disable-sync',
-      // '--hide-scrollbars',
-      // '--ignore-gpu-blacklist',
-      // '--metrics-recording-only',
-      // '--mute-audio',
-      // '--no-default-browser-check',
-      // '--no-first-run',
-      // '--no-pings',
-      // '--no-sandbox',
-      // '--no-zygote',
-      // '--password-store=basic',
-      // '--use-gl=swiftshader',
-      // '--use-mock-keychain',
+      '--disable-background-networking',
+      '--disable-background-timer-throttling',
+      '--disable-backgrounding-occluded-windows',
+      '--disable-breakpad',
+      '--disable-client-side-phishing-detection',
+      '--disable-component-update',
+      '--disable-default-apps',
+      '--disable-dev-shm-usage',
+      '--disable-domain-reliability',
+      '--disable-extensions',
+      '--disable-features=AudioServiceOutOfProcess',
+      '--disable-hang-monitor',
+      '--disable-ipc-flooding-protection',
+      '--disable-notifications',
+      '--disable-offer-store-unmasked-wallet-cards',
+      '--disable-popup-blocking',
+      '--disable-print-preview',
+      '--disable-prompt-on-repost',
+      '--disable-renderer-backgrounding',
+      '--disable-setuid-sandbox',
+      '--disable-speech-api',
+      '--disable-sync',
+      '--hide-scrollbars',
+      '--ignore-gpu-blacklist',
+      '--metrics-recording-only',
+      '--mute-audio',
+      '--no-default-browser-check',
+      '--no-first-run',
+      '--no-pings',
+      '--no-sandbox',
+      '--no-zygote',
+      '--password-store=basic',
+      '--use-gl=swiftshader',
+      '--use-mock-keychain',
     ];
 
     this.browser = await chromium.puppeteer.launch({
@@ -119,44 +119,36 @@ const classMap = {
 
 module.exports.handler = async (event, context) => {
 
-  try{
-    const url=event["queryStringParameters"]["url"];
-    const words = url.split('/')[2];
-    const ClassConstructor = classMap[words];
+  
+  const url=event["queryStringParameters"]["url"];
+  // const words = url.split('/')[2];
+  // const ClassConstructor = classMap[words];
 
-    let web=null;
-    // Create an instance of the class constructor if it exists
-    if (ClassConstructor) {
-      web = new ClassConstructor(url);
-      // do something with web...
-    }else{
-      return {
-        statusCode: 403,
-        message: "데이터에 없는 url 입니다. ("+words+")"
-      }
-    }
-    
-    
-    await web.chrome_on();
-    await web.page_on();
-    await web.page_goto();
-    const comments = await web.crawling();
-    await web.chrome_close();
-  }
-  catch(err){  
-    return {
-      statusCode: 503,
-      body: JSON.stringify({
-        message: err.name,
-      })
-    }
-  }
-  //setTimeout(() => chrome.instance.kill(), 0);
+  // let web=null;
+  // // Create an instance of the class constructor if it exists
+  // if (ClassConstructor) {
+  //   web = new ClassConstructor(url);
+  //   // do something with web...
+  // }else{
+  //   return {
+  //     statusCode: 403,
+  //     message: "데이터에 없는 url 입니다. ("+words+")"
+  //   }
+  // }
+  
+  
+  // await web.chrome_on();
+  // await web.page_on();
+  // await web.page_goto();
+  // const comments = await web.crawling();
+  // await web.chrome_close();
+
+  // //setTimeout(() => chrome.instance.kill(), 0);
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: comments,
+      message: url,
     })
   }
 
